348af2726213f6177f15cb44513863378d1d6afe
KEITH\keith <wongdaokang@gmail.com>
2016-10-29 11:31:49 +0800

    First Commit

---
+++ AI_Framework/Base/Source/SceneText.cpp
@@ -0,0 +1,201 @@
+#include "SceneText.h"
+#include "GL\glew.h"
+
+#include "shader.hpp"
+#include "MeshBuilder.h"
+#include "Application.h"
+#include "Utility.h"
+#include "LoadTGA.h"
+#include <sstream>
+#include "KeyboardController.h"
+#include "MouseController.h"
+#include "SceneManager.h"
+#include "GraphicsManager.h"
+#include "ShaderProgram.h"
+#include "EntityManager.h"
+
+#include "GenericEntity.h"
+#include "GroundEntity.h"
+#include "TextEntity.h"
+#include "SpriteEntity.h"
+#include "Light.h"
+#include "SkyBox/SkyBoxEntity.h"
+
+#include <iostream>
+using namespace std;
+
+SceneText* SceneText::sInstance = new SceneText(SceneManager::GetInstance());
+
+SceneText::SceneText()
+{
+}
+
+SceneText::SceneText(SceneManager* _sceneMgr)
+{
+	_sceneMgr->AddScene("Start", this);
+}
+
+SceneText::~SceneText()
+{
+}
+
+void SceneText::Init()
+{
+	currProg = GraphicsManager::GetInstance()->LoadShader("default", "Shader//Texture.vertexshader", "Shader//Texture.fragmentshader");
+
+	// Tell the shader program to store these uniform locations
+	currProg->AddUniform("MVP");
+	currProg->AddUniform("MV");
+	currProg->AddUniform("MV_inverse_transpose");
+	currProg->AddUniform("material.kAmbient");
+	currProg->AddUniform("material.kDiffuse");
+	currProg->AddUniform("material.kSpecular");
+	currProg->AddUniform("material.kShininess");
+	currProg->AddUniform("lightEnabled");
+	currProg->AddUniform("numLights");
+	currProg->AddUniform("lights[0].type");
+	currProg->AddUniform("lights[0].position_cameraspace");
+	currProg->AddUniform("lights[0].color");
+	currProg->AddUniform("lights[0].power");
+	currProg->AddUniform("lights[0].kC");
+	currProg->AddUniform("lights[0].kL");
+	currProg->AddUniform("lights[0].kQ");
+	currProg->AddUniform("lights[0].spotDirection");
+	currProg->AddUniform("lights[0].cosCutoff");
+	currProg->AddUniform("lights[0].cosInner");
+	currProg->AddUniform("lights[0].exponent");
+	currProg->AddUniform("lights[1].type");
+	currProg->AddUniform("lights[1].position_cameraspace");
+	currProg->AddUniform("lights[1].color");
+	currProg->AddUniform("lights[1].power");
+	currProg->AddUniform("lights[1].kC");
+	currProg->AddUniform("lights[1].kL");
+	currProg->AddUniform("lights[1].kQ");
+	currProg->AddUniform("lights[1].spotDirection");
+	currProg->AddUniform("lights[1].cosCutoff");
+	currProg->AddUniform("lights[1].cosInner");
+	currProg->AddUniform("lights[1].exponent");
+	currProg->AddUniform("colorTextureEnabled");
+	currProg->AddUniform("colorTexture");
+	currProg->AddUniform("textEnabled");
+	currProg->AddUniform("textColor");
+
+	// Tell the graphics manager to use the shader we just loaded
+	GraphicsManager::GetInstance()->SetActiveShader("default");
+
+	lights[0] = new Light();
+	GraphicsManager::GetInstance()->AddLight("lights[0]", lights[0]);
+	lights[0]->type = Light::LIGHT_DIRECTIONAL;
+	lights[0]->position.Set(0, 20, 0);
+	lights[0]->color.Set(1, 1, 1);
+	lights[0]->power = 1;
+	lights[0]->kC = 1.f;
+	lights[0]->kL = 0.01f;
+	lights[0]->kQ = 0.001f;
+	lights[0]->cosCutoff = cos(Math::DegreeToRadian(45));
+	lights[0]->cosInner = cos(Math::DegreeToRadian(30));
+	lights[0]->exponent = 3.f;
+	lights[0]->spotDirection.Set(0.f, 1.f, 0.f);
+	lights[0]->name = "lights[0]";
+
+	currProg->UpdateInt("numLights", 1);
+	currProg->UpdateInt("textEnabled", 0);
+
+	// Create the playerinfo instance, which manages all information about the player
+	playerInfo = CPlayerInfo::GetInstance();
+	playerInfo->Init();
+
+	// Create and attach the camera to the scene
+	//camera.Init(Vector3(0, 0, 10), Vector3(0, 0, 0), Vector3(0, 1, 0));
+	camera.Init(playerInfo->GetPos(), playerInfo->GetTarget(), playerInfo->GetUp());
+	//playerInfo->AttachCamera(&camera);
+
+	//GraphicsManager::GetInstance()->AttachCamera(&camera);
+
+	// Load all the meshes
+	MeshBuilder::GetInstance()->GenerateAxes("reference");
+	MeshBuilder::GetInstance()->GenerateCrossHair("crosshair");
+	MeshBuilder::GetInstance()->GenerateQuad("quad", Color(1, 1, 1), 1.f);
+	MeshBuilder::GetInstance()->GetMesh("quad")->textureID = LoadTGA("Image//calibri.tga");
+	MeshBuilder::GetInstance()->GenerateText("text", 16, 16);
+	MeshBuilder::GetInstance()->GetMesh("text")->textureID = LoadTGA("Image//calibri.tga");
+	MeshBuilder::GetInstance()->GetMesh("text")->material.kAmbient.Set(1, 0, 0);
+
+	// Create entities into the scene
+	Create::Entity("reference", Vector3(0.0f, 0.0f, 0.0f)); // Reference
+	Create::Sprite2DObject("crosshair", Vector3(0.0f, 0.0f, 0.0f), Vector3(10.0f, 10.0f, 10.0f));
+
+	// Setup the 2D entities
+	float halfWindowWidth = Application::GetInstance().GetWindowWidth() / 2.0f;
+	float halfWindowHeight = Application::GetInstance().GetWindowHeight() / 2.0f;
+	float fontSize = 25.0f;
+	float halfFontSize = fontSize / 2.0f;
+	for (int i = 0; i < 3; ++i)
+	{
+		textObj[i] = Create::Text2DObject("text", Vector3(-halfWindowWidth, -halfWindowHeight + fontSize*i + halfFontSize, 0.0f), "", Vector3(fontSize, fontSize, fontSize), Color(0.0f, 1.0f, 0.0f));
+	}
+	textObj[0]->SetText("HELLO WORLD");
+}
+
+void SceneText::Update(double dt)
+{
+	// Update our entities
+	EntityManager::GetInstance()->Update(dt);
+
+	// Update the player position and other details based on keyboard and mouse inputs
+	//playerInfo->Update(dt);
+
+	//camera.Update(dt); // Can put the camera into an entity rather than here (Then we don't have to write this)
+	//GraphicsManager::GetInstance()->UpdateLights(dt);
+
+	// Update the 2 text object values.
+	// Eg. FPSRenderEntity or inside RenderUI for LightEntity
+	std::ostringstream ss;
+	ss.precision(5);
+	float fps = (float)(1.f / dt);
+	ss << "FPS: " << fps;
+	textObj[1]->SetText(ss.str());
+
+	// Update the player position into textObj[2]
+	std::ostringstream ss1;
+	ss1.precision(4);
+	ss1 << "Player:" << playerInfo->GetPos();
+	textObj[2]->SetText(ss1.str());
+}
+
+void SceneText::Render()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	GraphicsManager::GetInstance()->UpdateLightUniforms();
+
+	// Setup 3D pipeline then render 3D
+	GraphicsManager::GetInstance()->SetPerspectiveProjection(45.0f, 4.0f / 3.0f, 0.1f, 10000.0f);
+	GraphicsManager::GetInstance()->AttachCamera(&camera);
+	EntityManager::GetInstance()->Render();
+
+	// Setup 2D pipeline then render 2D
+	int halfWindowWidth = Application::GetInstance().GetWindowWidth() / 2;
+	int halfWindowHeight = Application::GetInstance().GetWindowHeight() / 2;
+	GraphicsManager::GetInstance()->SetOrthographicProjection(-halfWindowWidth, halfWindowWidth, -halfWindowHeight, halfWindowHeight, -10, 10);
+	GraphicsManager::GetInstance()->DetachCamera();
+	EntityManager::GetInstance()->RenderUI();
+}
+
+void SceneText::Exit()
+{
+	// Detach camera from other entities
+	GraphicsManager::GetInstance()->DetachCamera();
+	playerInfo->DetachCamera();
+
+	if (playerInfo->DropInstance() == false)
+	{
+#if _DEBUGMODE ==1
+
+		cout << "Unable to drop PlayerInfo class" << endl;
+#endif
+	}
+
+	// Delete the lights
+	delete lights[0];
+}
